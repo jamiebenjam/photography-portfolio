fdf2c1082862ce8636218912815138e9c2eab5f2
Jamie Benjamin <bennythedev@gmail.com>
2023-04-05 14:08:29 -0500

    layout start

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,35 +13,32 @@ const Header = () => {
   return (
     <Navbar fixed="top" expand="lg" id="site-header">
       <Container className="nav-container">
-        <Navbar.Toggle aria-controls="site-header" />
-        <Navbar.Collapse className="nav-links">
-          <div className="nav-links-container">
-            <Nav className="nav-column">
-              <Nav.Link href="/" className="nav-logo">
-                jbp
-              </Nav.Link>
-              <Nav.Link href="portrait">portrait</Nav.Link>
-              <Nav.Link href="product">product</Nav.Link>
-              <Nav.Link href="people-product">people + product</Nav.Link>
-              <Nav.Link href="influencer">influencer</Nav.Link>
-              <Nav.Link href="couple">couple</Nav.Link>
-              <Nav.Link href="wedding">wedding</Nav.Link>
-              <Nav.Link href="event">event</Nav.Link>
-            </Nav>
-            <div className="social-container">
-              <Button variant="outline-dark" className="round-btn">
-                <a href="https://www.instagram.com/jamiebenjam/">
-                  <BsInstagram size={12} className="social-icons" />
-                </a>
-              </Button>
-              <Button variant="outline-dark" className="round-btn">
-                <a href="mailto:jamiebenjaminphotography@gmail.com">
-                  <MdOutlineMail size={12} className="social-icons" />
-                </a>
-              </Button>
-            </div>
+        <div className="nav-links-container">
+          <Nav className="nav-column">
+            <Nav.Link href="/" className="nav-logo">
+              jbp
+            </Nav.Link>
+            <Nav.Link href="portrait">portrait</Nav.Link>
+            <Nav.Link href="product">product</Nav.Link>
+            <Nav.Link href="people-product">people + product</Nav.Link>
+            <Nav.Link href="influencer">influencer</Nav.Link>
+            <Nav.Link href="couple">couple</Nav.Link>
+            <Nav.Link href="wedding">wedding</Nav.Link>
+            <Nav.Link href="event">event</Nav.Link>
+          </Nav>
+          <div className="social-container">
+            <Button variant="outline-dark" className="round-btn">
+              <a href="https://www.instagram.com/jamiebenjam/">
+                <BsInstagram size={12} className="social-icons" />
+              </a>
+            </Button>
+            <Button variant="outline-dark" className="round-btn">
+              <a href="mailto:jamiebenjaminphotography@gmail.com">
+                <MdOutlineMail size={12} className="social-icons" />
+              </a>
+            </Button>
           </div>
-        </Navbar.Collapse>
+        </div>
       </Container>
     </Navbar>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 import Header from '../components/Header';
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
+      <div className="gallery-container">
+        <div className="photo-row">
+          <div className="photo-column">
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1598562359929-JPVI0KN7WPUS02F52F8V/5D3_7521.jpg?format=750w" />
+          </div>
+          <div className="photo-column">
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1616259149306-ZIC6RXA3LM4OVQ3RFGZL/Untitled+Session6098-Edit.jpg?format=750w" />
+          </div>
+          <div className="photo-column">
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1614132337878-LQOLOOSCZ2YQI5DYS8E9/5D3_0491-Edit.jpg?format=2500w" />
+          </div>
+        </div>
+      </div>
+      <h1>test</h1>
     </div>
   );
 };
