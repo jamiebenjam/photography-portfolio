ec8b53052c79160651f5949b6315b72c537fbe38
Jamie Benjamin <bennythedev@gmail.com>
2023-04-05 14:17:41 -0500

    gallery container

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,22 @@ const LandingPage = () => {
         <div className="photo-row">
           <div className="photo-column">
             <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1598562359929-JPVI0KN7WPUS02F52F8V/5D3_7521.jpg?format=750w" />
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1598562359929-JPVI0KN7WPUS02F52F8V/5D3_7521.jpg?format=750w" />
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1598562359929-JPVI0KN7WPUS02F52F8V/5D3_7521.jpg?format=750w" />
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1598562359929-JPVI0KN7WPUS02F52F8V/5D3_7521.jpg?format=750w" />
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1598562359929-JPVI0KN7WPUS02F52F8V/5D3_7521.jpg?format=750w" />
           </div>
           <div className="photo-column">
             <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1616259149306-ZIC6RXA3LM4OVQ3RFGZL/Untitled+Session6098-Edit.jpg?format=750w" />
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1616259149306-ZIC6RXA3LM4OVQ3RFGZL/Untitled+Session6098-Edit.jpg?format=750w" />
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1616259149306-ZIC6RXA3LM4OVQ3RFGZL/Untitled+Session6098-Edit.jpg?format=750w" />
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1616259149306-ZIC6RXA3LM4OVQ3RFGZL/Untitled+Session6098-Edit.jpg?format=750w" />
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1616259149306-ZIC6RXA3LM4OVQ3RFGZL/Untitled+Session6098-Edit.jpg?format=750w" />
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1616259149306-ZIC6RXA3LM4OVQ3RFGZL/Untitled+Session6098-Edit.jpg?format=750w" />
           </div>
           <div className="photo-column">
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1614132337878-LQOLOOSCZ2YQI5DYS8E9/5D3_0491-Edit.jpg?format=2500w" />
+            <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1614132337878-LQOLOOSCZ2YQI5DYS8E9/5D3_0491-Edit.jpg?format=2500w" />
             <Image src="https://images.squarespace-cdn.com/content/v1/583f300f5016e1fe6892262b/1614132337878-LQOLOOSCZ2YQI5DYS8E9/5D3_0491-Edit.jpg?format=2500w" />
           </div>
         </div>
